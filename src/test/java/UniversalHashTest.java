14187f9a61bf401632fbc290883717eef39d4df7
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2019-08-14 22:11:30 +1000

    Some refactoring

--- src/test/java/UniversalHashTest.java
+++ src/test/java/UniversalHashTest.java
@@ -1,5 +1,5 @@
-public class UniversalHashTest {
-    static void doTest(String args[]) throws Exception { //for testing
+class UniversalHashTest {
+    static void doTest(String[] args) throws Exception { //for testing
         if(args.length != 2){
             System.err.println("should be two arguments: lengthOfMembershipArray numberOfHashFunctionsToUse");
             System.exit(0);
@@ -7,7 +7,7 @@ public class UniversalHashTest {
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
 
-        String ss[] = {"Tony","Matthias","Yadeesha","Shuo"};
+        String[] ss = {"Tony", "Matthias", "Yadeesha", "Shuo"};
         int m = 100;
         BloomFilter<String> sb = new BloomFilter(n,k);
         BloomFilter<Integer> si = new BloomFilter(n,k);
