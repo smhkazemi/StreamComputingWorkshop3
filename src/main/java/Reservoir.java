1a3379d4ca19481a5f3b441893f18f9625fa48e7
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2019-08-14 16:42:04 +1000

    Initial implementation of Reservior sampling algorithm

--- src/main/java/Reservoir.java
+++ src/main/java/Reservoir.java
@@ -1,35 +1,37 @@
-
+import java.util.Random;
 
 public class Reservoir{
-	private int[] sampleArray;
-	private int streamLength = 0;
-	private int numberOfSamplesToStoreFromTheStream;
+	private Object[] sampleArray;
+	private Random randomGenerator;
+	private int lastIndexOnSampleArrayToInsert;
+	private int numberOfSeenItemsInStream;
 	
-	public Reservoir(int numberOfSamplesToStoreFromTheStream){
-	    // constructor
+	Reservoir(int sampleNum){
+		sampleArray = new Object[sampleNum];
+		lastIndexOnSampleArrayToInsert = 0;
+		numberOfSeenItemsInStream = 0;
+		randomGenerator = new Random();
+		randomGenerator.setSeed(1234567890);
 	}
 	
-	public void considerItem(int x){
-		// to make things a bit simpler, focus on the case of sampling numberOfSamplesToStoreFromTheStream integers from a stream
+	public void considerItem(Object toConsider){
+		numberOfSeenItemsInStream++;
+		if(lastIndexOnSampleArrayToInsert < sampleArray.length)
+		{
+			sampleArray[lastIndexOnSampleArrayToInsert] = toConsider;
+			lastIndexOnSampleArrayToInsert++;
+		}
+		int randomlyGeneratedIndex = randomGenerator.nextInt(numberOfSeenItemsInStream);
+		if(randomlyGeneratedIndex < sampleArray.length)
+		{
+			sampleArray[randomlyGeneratedIndex] =  toConsider;
+		}
 	}
 	
-	public int report(int S[]){
-		// tell us what's inside, by copying into S
-		// use the returned int to say how many integers are in the sample! (what if n<numberOfSamplesToStoreFromTheStream?!)
-		
-		// actually, this is a very ugly C-style way of doing this.
-		return 0;
-	}
-	
-	public static void main(String args[]){
-		int i;
-		Reservoir r;
-		int f;
-		int k = 10; // for example -- feel free to change this!
-		int S[] = new int[k];
-		
-		r = new Reservoir(k);
-
-		// How do you test your code?
+	public Object[] report()
+	{
+		Object[] resualt = new Object[lastIndexOnSampleArrayToInsert];
+		System.arraycopy(sampleArray, 0, resualt, 0, lastIndexOnSampleArrayToInsert);
+		return resualt;
 	}
 }
---
+++ src/test/java/ReserviorTest.java
@@ -0,0 +1,13 @@
+public class ReserviorTest {
+    public static void main(){
+        int i;
+        Reservoir r;
+        int f;
+        int k = 10; // for example -- feel free to change this!
+        int S[] = new int[k];
+
+        r = new Reservoir(k);
+
+        // How do you test your code?
+    }
+}
