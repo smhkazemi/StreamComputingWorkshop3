9d8a7d005105b7025cccbd4c60e14d0aafb61715
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2019-08-21 14:35:06 +1000

    - Minor refactoring happened

--- src/test/java/ChiSquareTest.java
+++ src/test/java/ChiSquareTest.java
@@ -12,8 +12,6 @@ class ChiSquareTest {
             ChiSquare.doTest(new Integer[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
                     new Integer[]{1, 1, 1, 10, 10, 1, 10, 10, 1, 10});
             throw new Exception("given: {1, 1, 1, 10, 10, 1, 10, 10, 1, 10}" + " expected to fail but passed");
-        } catch (Exception ignored) {
-
-        }
+        } catch (Exception ignored) {}
     }
 }
