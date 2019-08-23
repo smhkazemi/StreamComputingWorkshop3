7e60c0782f2972762b85b528ce2712dfd33a64b9
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2019-08-23 14:09:23 +1000

    - Changing variable types in MorrisCounter to short - Using less memory in MorrisCounter - Refactoring MorrisCounter - Way improving MorrisCounterTest

--- src/test/java/MorrisCounterTest.java
+++ src/test/java/MorrisCounterTest.java
@@ -1,22 +1,41 @@
+import ServiceClasses.ChiSquare.ChiSquare;
+
+import java.util.Arrays;
+
 class MorrisCounterTest
 {
     static void doTest(String[] args)
     {
-        int n = Integer.parseInt(args[0]);
-        int k = Integer.parseInt(args[1]);
-        MorrisCounter[] morrisCounters = new MorrisCounter[k];
-        for(int indexOnMorrisCounters = 0; indexOnMorrisCounters < k; indexOnMorrisCounters++)
+        int exactNumberOfAnItemInTheStream = Integer.parseInt(args[0]);
+        int numberOfMorrisCounters = Integer.parseInt(args[1]);
+        MorrisCounter[] morrisCounters = new MorrisCounter[numberOfMorrisCounters];
+        Integer[] expectedResults = new Integer[numberOfMorrisCounters];
+        Integer[] results = new Integer[numberOfMorrisCounters];
+        for(int indexOnMorrisCounters = 0; indexOnMorrisCounters < numberOfMorrisCounters; indexOnMorrisCounters++)
         {
             morrisCounters[indexOnMorrisCounters] = new MorrisCounter();
         }
-        for(int counter = 0; counter < n; counter++)
+        for(int counter = 0; counter < exactNumberOfAnItemInTheStream; counter++)
         {
             for(MorrisCounter itemInArray : morrisCounters)
             {
                 itemInArray.increment();
-                // ToDo: add exceptions
-                // System.out.println("actual "+i+" Morris["+j+"]"+ morrisCounters[j].myCount());
             }
+        }
+        for(int indexOnMorrisCounterArray = 0; indexOnMorrisCounterArray < numberOfMorrisCounters;
+            indexOnMorrisCounterArray++)
+        {
+            expectedResults[indexOnMorrisCounterArray] = exactNumberOfAnItemInTheStream;
+            results[indexOnMorrisCounterArray] =
+                    morrisCounters[indexOnMorrisCounterArray].estimatedCount();
+        }
+        try {
+            ChiSquare.doTest(expectedResults, results);
+        }catch (Exception e) {
+            System.out.println("numberOfMorrisCounters: " + numberOfMorrisCounters
+                    + " exactNumberOfAnItemInTheStream: " + exactNumberOfAnItemInTheStream);
+            System.out.println("expected: " + Arrays.toString(expectedResults)
+                    + " \nfound: " + Arrays.toString(results));
         }
     }
 }
