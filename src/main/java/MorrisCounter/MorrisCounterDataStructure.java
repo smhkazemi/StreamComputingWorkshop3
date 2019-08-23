6d64c5271a43e4f1f1b92e3756ed6ccf817ea068
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2019-08-23 17:10:16 +1000

    Creating MorrisCounterDataStructure class t

---
+++ src/main/java/MorrisCounter/MorrisCounterDataStructure.java
@@ -0,0 +1,57 @@
+package MorrisCounter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class MorrisCounterDataStructure {
+
+    private MorrisCounterInterface[][]
+            morrisCounters;
+
+    public MorrisCounterDataStructure(Double probabilityOfBadEstimate,
+                               Double factorOfDeferenceWithExactSolution)
+    {
+        morrisCounters =
+                new MorrisCounterNotStoringEstimatedValue
+                        [(int) (12D * Math.log(1D / probabilityOfBadEstimate)) + 1]
+                        [(int) (2D / Math.pow(2, factorOfDeferenceWithExactSolution)) + 1];
+        for(int indexOnRow = 0; indexOnRow < morrisCounters.length; indexOnRow++)
+        {
+            for(int indexOnColumn = 0; indexOnColumn < morrisCounters[0].length; indexOnColumn++)
+            {
+                morrisCounters[indexOnRow][indexOnColumn] = new MorrisCounterNotStoringEstimatedValue();
+            }
+        }
+    }
+
+    public void increment()
+    {
+        for (MorrisCounterInterface[] row : morrisCounters)
+        {
+            for (int indexOnColumn = 0; indexOnColumn < morrisCounters[0].length; indexOnColumn++)
+            {
+                row[indexOnColumn].increment();
+            }
+        }
+    }
+
+    public Double getEstimate()
+    {
+        ArrayList<Double> meanValuesOfRows = new ArrayList<>();
+        Double meanForCurrentRow = 0D;
+        int numberOfColumns = morrisCounters[0].length;
+        for (MorrisCounterInterface[] morrisCounter : morrisCounters)
+        {
+            for (int indexOnColumn = 0; indexOnColumn < numberOfColumns; indexOnColumn++)
+            {
+                meanForCurrentRow += morrisCounter[indexOnColumn].getEstimate();
+            }
+            meanForCurrentRow /= numberOfColumns;
+            meanValuesOfRows.add(meanForCurrentRow);
+            meanForCurrentRow = 0D;
+        }
+        meanValuesOfRows.sort(Comparator.naturalOrder());
+        return meanValuesOfRows.get(meanValuesOfRows.size() >> 1);
+    }
+
+}
